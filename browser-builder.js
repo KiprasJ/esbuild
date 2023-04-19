b14010a3e34d08fb5efeb8568fbeb4021ef439ef
Kipras Jankauskas <kipras@codigi.lt>
2023-04-19 11:16:51 +0300

    initial commit

---
+++ browser-builder.js
@@ -0,0 +1,20 @@
+const esbuild = require('esbuild')
+const { sassPlugin } = require('esbuild-sass-plugin')
+const inlineImage = require('esbuild-plugin-inline-image');
+
+esbuild.build({
+    entryPoints: ['src/App.tsx'],
+    bundle: true,
+    minify: true,
+    target: ['esnext'],
+    outdir: 'out',
+    plugins: [sassPlugin({
+        cssImports: true,
+        importMapper: (path) => {
+            if(path === 'src/globals/global') {
+                return path.replace(path, './src/globals/global.scss')
+            }
+            return path
+        }
+    }), inlineImage()]
+})
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import styles from './App.module.scss';
+import './globals/static.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className={styles.container}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.tsx</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+      </div>
   );
 }
 
